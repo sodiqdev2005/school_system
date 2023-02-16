55416cadd680ee8bfd5bced4832da461318c43fc
Sodiq <86997572+sodiqoydinov@users.noreply.github.com>
2023-02-16 20:25:15 +0500

    Yangilandi
    Yangilandi

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a6@uzun%ufht)%+kzb(3kh7#%k3b@$g))na#!k0!z7c*a@cq9&'
+SECRET_KEY = 'Bu yerda sizning secret keyingiz turadi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
